34f4625134237ebca1c7383862cce81347c23fbf
Enrique Cartagena <enriquecartagena@gmail.com>
2021-04-25 16:41:18 +0200

    Moves method from controller to model

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,18 +1,6 @@
 class BooksController < ApplicationController
   def index
-    @books = books_with_availability
+    @books = Book::books_with_availability
     render json: @books
   end
-end
-
-private
-
-def books_with_availability()
-  query_raw = <<~SQL
-    SELECT b.id, b.title, b.pages, b.isbn, b.author_id, a.name AS author_name, b.copies, 
-    b.copies - (SELECT count('x') FROM lendings le WHERE le.book_id = b.id AND returned_at IS NULL) AS copies_available
-      FROM books b
-      JOIN authors a ON a.id = b.author_id;
-    SQL
-  ActiveRecord::Base.connection_pool.with_connection { |con| con.exec_query(query_raw) }
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,13 @@
 class Book < ApplicationRecord
   belongs_to :author
+
+  def self.books_with_availability()
+    query_raw = <<~SQL
+      SELECT b.id, b.title, b.pages, b.isbn, b.author_id, a.name AS author_name, b.copies, 
+      b.copies - (SELECT count('x') FROM lendings le WHERE le.book_id = b.id AND returned_at IS NULL) AS copies_available
+        FROM books b
+        JOIN authors a ON a.id = b.author_id;
+      SQL
+    ActiveRecord::Base.connection_pool.with_connection { |con| con.exec_query(query_raw) }
+  end
 end
